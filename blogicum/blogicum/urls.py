104b6a2152880177b08aefc02b2481da6ca43999
PRESNOVODNOE <77174152+itsAlexey@users.noreply.github.com>
2025-09-28 22:28:07 +0300

    Сломал

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from pages import views as error_views
 from django.conf import settings
 from django.conf.urls.static import static
-from pages import views as error_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("blog.urls", namespace="blog")),
     path("auth/", include("django.contrib.auth.urls")),
+    path("pages/", include("pages.urls", namespace="pages")),
 ]
 
 handler404 = error_views.page_not_found
